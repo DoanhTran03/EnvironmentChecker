571d2833a926c55d24a72eaac598f570f27d9244
Doanh Tran <doanhtran2003@gmail.com>
2024-01-13 19:59:38 -0800

    Update csv and export csv using ArrayList and  variable

--- index.ps1
+++ index.ps1
@@ -1,14 +1,20 @@
 $ServerList_Path = '.\ServerList.csv'
 $ServerList = Import-Csv -Path "$ServerList_Path" -Delimiter ','
+$Export = [System.Collections.ArrayList]@()
 
 foreach ($Server in $ServerList) {
-    $ServerName = $ServerName
-    $Connection = Test-Connection -TargetName $Server.Name -Count 1
+    $ServerName = $Server.Name
+    $LastStatus = $Server.$LastStatus
+    $Connection = Test-Connection $Server.Name -Count 1
 
     if ($Connection.Status -eq "Success") {
         Write-Host "$ServerName is online"
+        $Server.LastStatus = $LastStatus
     }
     else {
         Write-Host "$ServerName is offline"
+        $Server.LastStatus = $LastStatus
     }
+    $Export.Add($Server)
 }
+$Export | Export-Csv -Path .\ServerList.csv -Delimiter ','
